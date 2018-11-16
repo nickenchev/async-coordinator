f8a9d8db799bfe1a91ee75e103bafa10006757a8
Nick Enchev <nikolaenchev@gmail.com>
2018-11-16 11:42:51 -0500

    Removed garbage class

--- AsyncCoordinator/AsyncTask.h
+++
@@ -1,9 +0,0 @@
-//
-// Created by Nick Enchev on 2017-11-15.
-// Copyright (c) 2017 Rover Parking Inc. All rights reserved.
-//
-
-#import <Foundation/Foundation.h>
-
-@interface AsyncTask : NSObject
-@end
